762d46d84831d750184984efa1c0885f5119890b
Phill Coxon <phillcoxon@gmail.com>
2014-01-18 10:56:02 +1300

    Additional changes, started a loadwatch_ubuntu.sh version, read me improvements
    Signed-off-by: Phill Coxon <phillcoxon@gmail.com>

---
+++ bin/loadwatch_ubuntu.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+# let's add a version num for easier identification:
+# PAC version 0.1
+
+# NOTE:  Need to monitor logfiles as they will keep growing and need to be deleted
+
+#config
+FILE=loadwatch.`date +%F.%H.%M`
+DIR=/root/loadwatch
+COLUMNS=512
+SUBJECT="Loadwatch notification for $HOSTNAME at ".`date +%F.%H.%M`
+EMAILMESSAGE="/tmp/emailmessage.txt"
+
+# notification Email Address
+
+EMAIL="phillcoxon@gmail.com"
+
+#Load Threshold for doing a dump.
+THRESH=4
+
+#pull load average, log
+LOAD=`cat /proc/loadavg | awk '{print $1}' | awk -F '.' '{print $1}'`
+echo `date +%F.%X` - Load: $LOAD >> $DIR/checklog
+
+#trip
+if [ $LOAD -ge $THRESH ]
+then
+	#log 
+	echo Loadwatch tripped, dumping info to $DIR/$FILE >> $DIR/checklog
+	echo `date +%F.%H.%M` > $DIR/$FILE
+	echo "LoadWatch on $HOSTNAME triggered. Please Check it out." > $EMAILMESSAGE
+
+	#email (optional, set email address to customer and uncomment below lines)
+
+	#/bin/mail -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
+
+	#summary
+	echo -e "\n\nSummary------------------------------------------------------------\n\n" >> $DIR/$FILE
+        NUMHTTPD=`ps aux|grep httpd|wc -l`
+	echo "Number of HTTPD Processes: $NUMHTTPD" >> $DIR/$FILE
+	HTTPDCPU=`ps aux|grep httpd|awk '{sum+=$3} END {print sum}'`
+	echo "HTTPD CPU consumption: $HTTPDCPU %" >> $DIR/$FILE 
+	HTTPDMEM=`ps aux|grep httpd|awk '{sum+=$6} END {print sum}'`
+	HTTPDMEMMEG=$((HTTPDMEM/1024))
+	echo "HTTPD memory consumption: $HTTPDMEM Kilobytes ($HTTPDMEMMEG Megabytes)" >> $DIR/$FILE
+	NUMPROCS=`grep -c processor /proc/cpuinfo`
+	echo "Number of CPU Cores: $NUMPROCS" >> $DIR/$FILE
+	NUMPHP=`ps aux|grep php|wc -l`
+	echo "Number of PHP Processes: $NUMPHP" >> $DIR/$FILE
+	PHPCPU=`ps aux|grep php|awk '{sum+=$3} END {print sum}'`
+	echo "PHP CPU consumption: $PHPCPU %" >> $DIR/$FILE
+	PHPMEM=`ps aux|grep php|awk '{sum+=$6} END {print sum}'`
+	PHPMEMMEG=$((PHPMEM/1024))
+	echo "PHP memory consumption: $PHPMEM Kilobytes ($PHPMEMMEG Megabytes)" >> $DIR/$FILE
+	MYSQLCPU=`top -n 1 -S -b -U mysql|tail -n 2|head -n 1|awk {'print $9'}`
+	echo "MYSQL CPU consumption: $MYSQLCPU %" >> $DIR/$FILE
+	MYSQLMEM=`top -n 1 -S -b -U mysql|tail -n 2|head -n 1|awk {'print $6'}`
+	echo "MYSQL RAM consumption: $MYSQLMEM" >> $DIR/$FILE
+	uptime >> $DIR/$FILE
+	free -m >> $DIR/$FILE
+	echo " " >> $DIR/$FILE
+
+	echo '######## CPU top 20 ########' >> $DIR/$FILE
+        top -bcn1 | head -n 26 >> $DIR/$FILE
+	echo " " >> $DIR/$FILE
+
+	echo '######## Mem top 20 ########' >> $DIR/$FILE
+        top -bmcn1 | head -n 26 >> $DIR/$FILE
+	echo " " >> $DIR/$FILE
+
+	#mysql
+	echo -e "\n\nMySQL:------------------------------------------------------------\n\n" >> $DIR/$FILE
+	mysqladmin stat >> $DIR/$FILE
+	mysqladmin proc >> $DIR/$FILE
+
+	#apache
+	#echo -e "\n\nApache Full Status------------------------------------------------\n\n" >> $DIR/$FILE
+	#/sbin/service httpd fullstatus >> $DIR/$FILE
+
+	#network
+	echo -e "\n\nNetwork------------------------------------------------------------\n\n" >> $DIR/$FILE
+	netstat -tn 2>/dev/null | grep :80 | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head >> $DIR/$FILE
+	echo -e "\n\nNetwork 2----------------------------------------------------------\n\n" >> $DIR/$FILE
+	netstat -tn 2>/dev/null | grep :80 | awk '{print $5}' | cut -d: -f4 | sort | uniq -c | sort -nr | head >> $DIR/$FILE
+
+	#email
+	#echo -e "\n\nEmail------------------------------------------------------------\n\n" >> $DIR/$FILE
+	#EXIMQUEUE=`exim -bpc`
+	#echo "Exim Queue: $EXIMQUEUE " >> $DIR/$FILE 
+	#/usr/sbin/exiwhat >> $DIR/$FILE
+
+	#process list
+	echo -e "\n\nProcesses------------------------------------------------------------\n\n" >> $DIR/$FILE
+	ps auxf >> $DIR/$FILE
+
+ 	#Email the notification + summary
+
+    /usr/bin/mail -s "$SUBJECT" "$EMAIL" < $DIR/$FILE
+
+
+fi
--- readme.md
+++ readme.md
@@ -1,24 +1,47 @@
 #LoadWatch 
 
-## Email notification + diagnostics emailed when a preset server load is triggerd.  
+### Email notification + diagnostics emailed when a preset server load is triggerd.  
+
+* loadwatch.sh - for WHM/cPanel servers running on Centos
+* loadwatch_ubuntu.sh - ubuntu servers running nginx
 
 
 ### Install
 
-To install loadwatch.sh manually: 
+**To install loadwatch.sh manually:** 
 
-
-```mkdir /root/loadwatch
+```
+mkdir /root/loadwatch
 mkdir /root/loadwatch/bin/
 touch /root/loadwatch/bin/loadwatch.sh
 chmod 700 /root/loadwatch/bin/loadwatch.sh
-vim /root/loadwatch/bin/loadwatch.sh    #(copy and paste loadwatch.sh into here)
+vim /root/loadwatch/bin/loadwatch.sh    
 ```
+Copy and paste the contents of loadwatch.sh into vim and save. 
+
+**Using git:**
+
+```
+cd /root
+git clone https://github.com/getresults/loadwatch.git
+cd loadwatch/bin
+chmod 700 loadwatch.sh
+```
+Rename loadwatch_ubuntu.sh to loadwatch.sh if on an Ubuntu based server
+
+### Configure
+
+Edit loadwatch.sh and set the LOAD level and EMAIL address you want to use for notifications.
+
+For a single CPU server using '4' for the load level is fairly typical. 
+
+
+### Set up Cron
 
 Next, edit your crontab and insert the entry below which will run loadwatch.sh every 3 minutes to check server load and generate a report of the load is over the set threshold.
 
 ```
-crontab -e     #edit yor
+crontab -e  
 
 */3 * * * * /root/loadwatch/bin/loadwatch.sh > /dev/null 2>&1
 ```
@@ -26,10 +49,14 @@ crontab -e     #edit yor
 Make sure the loadwatch script is set to 700 permissions. 
 
 
-### Configure
+### Todo
 
-Edit loadwatch.sh and set the load level and notification email address you want to use. 
+* Output error if no EMAIL is specified
+* Add nginx / typical ubuntu email agent debug info to loadwatch_ubuntu.sh
+* Add brief info paragraphs to each section of the report so that users have a better idea of what is being displayed.
+* Look for ways to expand the script to provide more relevant info
 
-For a single CPU server using '4' for the load level is fairly typical. 
 
+### Acknowledgements
 
+* An awesome LiquidWeb.com staff member who gave me a copy of the base script.
